f5879ccac9a551ba5494ea046b1c4ca34503115c
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-20 10:13:05 +0700

    Update usernew.sh

--- udp-custom/ssh/usernew.sh
+++ udp-custom/ssh/usernew.sh
@@ -1,98 +1,65 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-echo "Checking VPS"
-sldomain=$(cat /root/nsdomain)
-slkey=$(cat /etc/slowdns/server.pub)
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+domain=$(cat /etc/xray/domain)
 clear
-cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
-if [ "$cekray" = "XRAY" ]; then
-domen=`cat /etc/xray/domain`
-else
-domen=`cat /etc/v2ray/domain`
-fi
-portsshws=`cat ~/log-install.txt | grep -w "SSH Websocket" | cut -d: -f2 | awk '{print $1}'`
-wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | awk '{print $1}'`
-
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;41;36m            SSH Account            \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -p "Username : " Login
-read -p "Password : " Pass
-read -p "Expired (hari): " masaaktif
-
-IP=$(curl -sS ifconfig.me);
-ossl=`cat /root/log-install.txt | grep -w "OpenVPN" | cut -f2 -d: | awk '{print $6}'`
-opensh=`cat /root/log-install.txt | grep -w "OpenSSH" | cut -f2 -d: | awk '{print $1}'`
-db=`cat /root/log-install.txt | grep -w "Dropbear" | cut -f2 -d: | awk '{print $1,$2}'`
-ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
-sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
-
-OhpSSH=`cat /root/log-install.txt | grep -w "OHP SSH" | cut -d: -f2 | awk '{print $1}'`
-OhpDB=`cat /root/log-install.txt | grep -w "OHP DBear" | cut -d: -f2 | awk '{print $1}'`
-OhpOVPN=`cat /root/log-install.txt | grep -w "OHP OpenVPN" | cut -d: -f2 | awk '{print $1}'`
-pkill sldns-server
-pkill sldns-client
-systemctl daemon-reload
-systemctl stop client-sldns
-systemctl stop server-sldns
-systemctl enable client-sldns
-systemctl enable server-sldns
-systemctl start client-sldns
-systemctl start server-sldns
-systemctl restart client-sldns
-systemctl restart server-sldns
+read -p "Username       : " Login
+read -p "Password       : " Pass
+read -p "Max IP         : " iplimit
+read -p "Expired (Days) : " masaaktif
 sleep 1
-clear
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-PID=`ps -ef |grep -v grep | grep sshws |awk '{print $2}'`
-
-if [[ ! -z "${PID}" ]]; then
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "⟨ SSH OVPN Account ⟩" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "Username    : $Login" | tee -a /etc/log-create-ssh.log
-echo -e "Password    : $Pass" | tee -a /etc/log-create-ssh.log
-echo -e "Expired On  : $exp" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "» IP          : $IP" | tee -a /etc/log-create-ssh.log
-echo -e "» Host        : $domen" | tee -a /etc/log-create-ssh.log
-echo -e "» OpenSSH     : $opensh" | tee -a /etc/log-create-ssh.log
-echo -e "» SSH WS      : $portsshws" | tee -a /etc/log-create-ssh.log
-echo -e "» SSH SSL WS  : $wsssl" | tee -a /etc/log-create-ssh.log
-echo -e "» SSL/TLS     :$ssl" | tee -a /etc/log-create-ssh.log
-echo -e "» UDPGW       : 7100-7900" | tee -a /etc/log-create-ssh.log
-echo -e "» UDP CUSTOM  : 1-65535" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "» Payload WSS : GET wss://isi_bug_disini HTTP/1.1[crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "» Payload WS  : GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
+#limitip
+if [[ $iplimit -gt 0 ]]; then
+echo -e "$iplimit" > /etc/funny/limit/ssh/ip/$user
 else
-
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "⟨ SSH OVPN Account ⟩" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "Username    : $Login" | tee -a /etc/log-create-ssh.log
-echo -e "Password    : $Pass" | tee -a /etc/log-create-ssh.log
-echo -e "Expired On  : $exp" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "» IP          : $IP" | tee -a /etc/log-create-ssh.log
-echo -e "» Host        : $domen" | tee -a /etc/log-create-ssh.log
-echo -e "» OpenSSH     : $opensh" | tee -a /etc/log-create-ssh.log
-echo -e "» SSH WS      : $portsshws" | tee -a /etc/log-create-ssh.log
-echo -e "» SSH SSL WS  : $wsssl" | tee -a /etc/log-create-ssh.log
-echo -e "» SSL/TLS     :$ssl" | tee -a /etc/log-create-ssh.log
-echo -e "» UDPGW       : 7100-7900" | tee -a /etc/log-create-ssh.log
-echo -e "» UDP CUSTOM  : 1-65535" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "» Payload WSS : GET wss://isi_bug_disini HTTP/1.1[crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
-echo -e "» Payload WS  : GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]" | tee -a /etc/log-create-ssh.log
-echo -e "◇━━━━━━━━━━━━━◇" | tee -a /etc/log-create-ssh.log
+echo > /dev/null
 fi
-echo "" | tee -a /etc/log-create-ssh.log
+clear
+systemctl restart ws-tls
+systemctl restart ws-nontls
+systemctl restart ssh-ohp
+systemctl restart dropbear-ohp
+systemctl restart openvpn-ohp
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
+echo -e "◇━━━━━━━━━━━━━◇"
+echo -e "  ⟨ SSH OVPN Account ⟩"
+echo -e "◇━━━━━━━━━━━━━◇"
+echo -e "» Username   : $Login"
+echo -e "» Password   : $Pass"
+echo -e "» Expired On : $exp"
+echo -e "◇━━━━━━━━━━━━━◇"
+echo -e "» IP         : $IP"
+echo -e "» Host       : $domen"
+echo -e "» OpenSSH    : $opensh"
+echo -e "» Dropbear   : $db"
+echo -e "» SSH WS     : $portsshws"
+echo -e "» SSH SSL WS : $wsssl"
+echo -e "» SSL/TLS    :$ssl"
+echo -e "» UDPGW      : 7100-7900"
+echo -e "» UDP Custom : 1-65350"
+echo -e "» Port NS    : ALL Port (22, 443, 143)"
+echo -e "» Nameserver : $sldomain"
+echo -e "» Pubkey     : $slkey"
+echo -e "» UDP CUSTOM  : 1-65535"
+echo -e "◇━━━━━━━━━━━━━◇"
+echo -e "» Payload WSS: GET wss://isi_bug_disini HTTP/1.1[crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "◇━━━━━━━━━━━━━◇"
+echo -e "» Payload WS : GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "◇━━━━━━━━━━━━━◇"
+fi
+echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 m-sshovpn
-
