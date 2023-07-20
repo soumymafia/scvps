cc7d0e26f1e8f51b2bf31b07969050e2b7975418
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-20 18:19:17 +0700

    Update menu-bot.sh

--- menu/menu-bot.sh
+++ menu/menu-bot.sh
@@ -3,8 +3,8 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/xolvaid/simplepanel.git
-unzip simplepanel/xolpanel.zip
+git clone https://github.com/Zeadxt/dbasu.git
+unzip dbasu/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
 
