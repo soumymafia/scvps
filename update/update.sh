c403b3a2e4ccf8ec354b260f9f611228d0488cef
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-21 21:24:03 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -3,5 +3,4 @@
 echo -e "Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/backup.sh" && chmod +x /usr/bin/menu-backup
 sleep 2
