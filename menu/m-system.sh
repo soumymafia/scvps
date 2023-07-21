512bfc060aa5a406ec951fb3577371e87aab0449
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-21 18:22:27 +0700

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -13,7 +13,7 @@ echo -e " [\e[36m•4\e[0m] Restart All Service"
 echo -e " [\e[36m•5\e[0m] Cek Bandwith"
 echo -e " [\e[36m•6\e[0m] Install TCP BBR"
 echo -e " [\e[36m•7\e[0m] DNS CHANGER"
-echo -e " [\e[36m•8\e[0m] UPDATE MENU"
+echo -e " [\e[36m•8\e[0m] BACKUP"
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
@@ -31,8 +31,7 @@ case $opt in
 5) clear ; bw ; exit ;;
 6) clear ; m-tcp ; exit ;;
 7) clear ; m-dns ; exit ;;
-8) clear ; wget /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/AutoScriptXray/master/menu/menu.sh" && chmod +x /usr/bin/menu ; reboot ;;
-0) clear ; menu ; exit ;;
+8) clear ; nano /usr/bin/backup ; exit 
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
 esac
