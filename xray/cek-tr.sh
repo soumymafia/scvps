a2b59be6a585ef9a1ef1a54aee15da7ce2abe23e
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-22 15:47:55 +0700

    Update cek-tr.sh

--- xray/cek-tr.sh
+++ xray/cek-tr.sh
@@ -1,39 +1,41 @@
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '###' | cut -d ' ' -f 2 | sort | uniq`);
-echo "-------------------------------";
-echo "-----=[ XRAY User Login ]=-----";
-echo "-------------------------------";
+data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC} ${COLBG1}            ${WH} TROJAN ONLINE NOW               ${NC} $COLOR1 $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
-echo -n > /tmp/ipxray.txt
+
+echo -n > /tmp/iptrojan.txt
 data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
+
 jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/ipxray.txt
+echo "$jum" >> /tmp/iptrojan.txt
 else
 echo "$ip" >> /tmp/other.txt
 fi
-jum2=$(cat /tmp/ipxray.txt)
+jum2=$(cat /tmp/iptrojan.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-jum=$(cat /tmp/ipxray.txt)
+
+jum=$(cat /tmp/iptrojan.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/ipxray.txt | nl)
-lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-echo -e "user :${GREEN} ${akun} ${NC}
-${RED}Online Jam ${NC}: ${lastlogin} wib";
-echo -e "$jum2";
-echo "-------------------------------"
+jum2=$(cat /tmp/iptrojan.txt | nl)
+echo -e "$COLOR1 ${NC}user: $akun";
+echo -e "${COLOR1}${NC}$jum2";
 fi
-rm -rf /tmp/ipxray.txt
+rm -rf /tmp/iptrojan.txt
 done
 rm -rf /tmp/other.txt
 echo ""
