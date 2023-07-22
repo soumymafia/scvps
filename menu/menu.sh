277ac746b3dcfe657149f103e9f43f3cc3565352
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-22 10:52:50 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -195,7 +195,7 @@ echo -e "                         MENU" | lolcat
 echo -e ""
 echo -e "[\e[36m1\e[0m]☞ Menu SSH ${Blue}\e[34m$ressh""      ""[\e[36m4\e[0m]☞ Menu Trojan ${Blue}\e[34m$resv2r"
 echo -e "[\e[36m2\e[0m]☞ Menu Vmess ${Blue}\e[34m$resv2r""    ""[\e[36m5\e[0m]☞ Menu Shadowsocks ${Blue}\e[34m$resv2r" 
-echo -e "[\e[36m3\e[0m]☞ Menu Vless ${Blue}\e[34m$resv2r""     ""[\e[36m6\e[0m]✉ Menu setting"
+echo -e "[\e[36m3\e[0m]☞ Menu Vless ${Blue}\e[34m$resv2r""    ""[\e[36m6\e[0m]✉ Menu setting"
 echo -e   ""
 echo -e   ""
 read -p " Select menu :  "  opt
