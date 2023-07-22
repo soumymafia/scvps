335de1ed881af25c7704a3b13c4cc3dd96da0a35
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-22 12:18:45 +0700

    Update backup.sh

--- ssh/backup.sh
+++ ssh/backup.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
+
+echo "Input your bot token: "; read token
+echo "Input your telegram user/group id: "; read telid
+
+rm /usr/bin/bkp
+echo -e "#!/bin/bash
+TELEGRAM_BOT_TOKEN=$token
+TELEGRAM_CHAT_ID=$telid
+
 clear
 IP=$(wget -qO- ipinfo.io/ip);
-date=$(date +"%Y-%m-%d")
+date=$(date +\"%Y-%m-%d\")
+
 clear
-email=$(cat /home/email)
-if [[ "$email" = "" ]]; then
-echo "Masukkan Email Untuk Menerima Backup"
-read -rp "Email : " -e email
-cat <<EOF>>/home/email
-$email
-EOF
-fi
-clear
-echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
+echo \"Mohon Menunggu, Proses Backup sedang berlangsung !!\"
 rm -rf /root/backup
 mkdir /root/backup
 cp /etc/passwd backup/
@@ -36,27 +26,28 @@ cp -r /etc/xray backup/xray
 cp -r /home/vps/public_html backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date.zip dr:backup/
-url=$(rclone link dr:backup/$IP-$date.zip)
-id=(`echo $url | grep '^https' | cut -d'=' -f2`)
-link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "
-Detail Backup 
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-" | mail -s "Backup Data" $email
-rm -rf /root/backup
-rm -r /root/$IP-$date.zip
-clear
-echo -e "
-Detail Backup 
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-"
-echo "BACKUP BERHASIL SILAHKAN SALIN LINK DIATAS"
+
+BACKUP_FILE=/root/backup/$IP-$date.zip
+
+if [[ \"$1\" == \"backup\" ]]; then
+    if [[ -f \"$BACKUP_FILE\" ]]; then
+        curl -F chat_id=\"$TELEGRAM_CHAT_ID\" -F document=@\"$BACKUP_FILE\" https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendDocument
+        echo -e \"Autobackup successfull! Check your telegram message! Press /start in your bot if you didnt get the file!
+        Upload your backup file to google drive if you want to restore the backup file! Cheer!\"
+        echo \"Press 1 to make the autobackup runs every day (at 00.01 Jakarta time)\"
+        echo \"Press 0 to ignore and just exit\"
+        read \"Your choose: \"chos
+        if [ \"$chos\" = 1 ]; then
+            echo -e \"1 0 1 * * root bkp backup\" >> /etc/crontab
+        elif [ \"$chos\" = 0 ]; then
+            menu
+        else
+            echo \"Choose only 1 or 0!\"
+            exit 1
+    else
+        echo \"Backup file not found.\"
+    fi
+else
+    echo \"Invalid command. Usage: bkp backup\"
+fi" > /usr/bin/bkp
+chmod +x /usr/bin/bkp
