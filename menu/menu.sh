b84262ff67372d7977eb0b8bbb49a8d5c483b17c
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-22 11:25:45 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -190,12 +190,12 @@ echo -e "   ${Blue}\033[0m ${Blue} $ssh1      $vma         $vla        $tra     
 echo -e "┌──────────────────────────────────────┐" | lolcat
 echo -e "\e[34m│\e[0mNGINX : \e[0m$resngx""  ""\e[0mXRAY : \e[0m$resv2r""  ""\e[0mDROPBEAR : \e[0m$resdbr"                                                                
 echo -e "└──────────────────────────────────────┘" | lolcat
-echo -e ""
-echo -e "                         MENU" | lolcat
-echo -e ""
+echo -e "$COLOR1 ${NC} ${COLBG1}${WH}             SYSTEM MENU ${NC} $COLOR1 $NC"                        
+echo -e "┌─────────────────────────────────────────────────────────────────────────────────────────┐"
 echo -e "[\e[36m1]\e[31m 私\e[0mMenu SSH ${Blue}\e[34m$ressh""      ""[\e[36m4]\e[35m 私\e[0mMenu Trojan ${Blue}\e[34m$resv2r"
 echo -e "[\e[36m2]\e[32m 私\e[0mMenu Vmess ${Blue}\e[34m$resv2r""    ""[\e[36m5]\e[91m 私\e[0mMenu Shadowsocks ${Blue}\e[34m$resv2r" 
 echo -e "[\e[36m3]\e[34m 私\e[0mMenu Vless ${Blue}\e[34m$resv2r""    ""[\e[36m6]\e[5m ぇ\e[0mMenu setting"
+echo -e "└─────────────────────────────────────────────────────────────────────────────────────────┘"
 echo -e   ""
 echo -e   ""
 read -p " Select menu :  "  opt
