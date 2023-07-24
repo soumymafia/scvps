8ed08e0b6ee2685022341f4b448017dfec5c698d
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-24 16:08:58 +0700

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-echo "Checking VPS"
+COLOR1='\033[0;35m'
+NC='\e[0m'
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m          • SYSTEM MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1  $NC$COLBG1               ${WH}• VPS SETTING •                 $COLOR1  $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}DOMAIN MENU           ${WH}[${COLOR1}06${WH}]${NC} ${COLOR1}• ${WH}DNS CHANGER"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH}SPEEDTEST        ${WH}[${COLOR1}07${WH}]${NC} ${COLOR1}• ${WH}MENU BACKUP"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH}AUTOREBOOT    ${WH}[${COLOR1}08${WH}]${NC} ${COLOR1}• ${WH}MENU RESTORE"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}RESTART${NC} $sts  ${WH}[${COLOR1}09${WH}]${NC} ${COLOR1}• ${WH}LOCK USER"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}BANDWIDTH         ${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}UNLOCK USER"
+echo -e " $COLOR1 $NC"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}MENU$NC"
+echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC}                 ${WH}• RVPN STORES •${NC}                 $COLOR1 $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Panel Domain"
-echo -e " [\e[36m•2\e[0m] Speedtest VPS"
-echo -e " [\e[36m•3\e[0m] Set Auto Reboot"
-echo -e " [\e[36m•4\e[0m] Restart All Service"
-echo -e " [\e[36m•5\e[0m] Cek Bandwith"
-echo -e " [\e[36m•6\e[0m] Install TCP BBR"
-echo -e " [\e[36m•7\e[0m] DNS CHANGER"
-echo -e " [\e[36m•8\e[0m] BACKUP"
-echo -e " [\e[36m•9\e[0m] RESTORE"
-echo -e " [\e[36m•10\e[0m] LOCK USER"
-echo -e " [\e[36m•11\e[0m] UNLOCK USER"
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo -e   ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p " Select menu : " opt
-echo -e ""
+echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
 case $opt in
 1) clear ; m-domain ; exit ;;
 2) clear ; speedtest ; exit ;;
 3) clear ; auto-reboot ; exit ;;
 4) clear ; restart ; exit ;;
 5) clear ; bw ; exit ;;
-6) clear ; m-tcp ; exit ;;
-7) clear ; m-dns ; exit ;;
-8) clear ; /usr/bin/backup ; exit ;;
-9) clear ; /usr/bin/restore ; exit ;;
-10) clear ; user-lock ; exit ;;
-11) clear ; user-unlock ; exit ;;
+6) clear ; m-dns ; exit ;;
+7) clear ; /usr/bin/backup ; exit ;;
+8) clear ; /usr/bin/restore ; exit ;;
+9) clear ; user-lock ; exit ;;
+10) clear ; user-unlock ; exit ;;
+0) clear ; menu ;; 
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
 esac
