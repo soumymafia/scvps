d90d868ae1d256954d6efcbfeedbe0cf703190be
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-25 17:29:06 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -2,6 +2,5 @@
 #clear
 echo -e "Downloading Update File"
 sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/udp-custom/ssh/usernew.sh" && chmod +x /usr/bin/add-ws
+wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/udp-custom/ssh/usernew.sh" && chmod +x /usr/bin/usernew
 sleep 2
