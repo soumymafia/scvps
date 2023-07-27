955c0ad7e6ac0b1ab565808670017cc43f279091
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-27 21:51:52 +0700

    Add files via upload

---
+++ menu/1.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+echo "Checking VPS"
+#link izin ip vps
+url_izin='https://raw.githubusercontent.com/rizkyckj/izin/master/izin'
+
+#IP VPS
+ip_vps=$(curl -sS ifconfig.me)
+
+# Mendapatkan isi file izin.txt dari URL
+izin=$(curl -s "$url_izin")
+
+# Memeriksa apakah konten izin.txt berhasil didapatkan
+if [[ -n "$izin" ]]; then
+  while IFS= read -r line; do
+    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
+    nama=$(echo "$line" | awk '{print $1}')
+    ipvps=$(echo "$line" | awk '{print $2}')
+    tanggal=$(echo "$line" | awk '{print $3}')
+
+    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
+    if [[ "$ipvps" == "$ip_vps" ]]; then
+      echo "Nama VPS: $nama"
+      echo "IP VPS: $ipvps"
+      echo "Tanggal Kadaluwarsa: $tanggal"
+      break
+    fi
+  done <<< "$izin"
+
+  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
+  if [[ "$ipvps" != "$ip_vps" ]]; then
+    echo "IP VPS tidak ditemukan dalam izin.txt"
+    exit 0
+  fi
+else
+  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
+  exit 0
+fi
+
+vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
+let vla=$vlx/2
+vmc=$(grep -c -E "^### " "/etc/xray/config.json")
+let vma=$vmc/2
+ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+
+trx=$(grep -c -E "^#! " "/etc/xray/config.json")
+let tra=$trx/2
+ssx=$(grep -c -E "^## " "/etc/xray/config.json")
+let ssa=$ssx/2
+trg=$(grep -c -E "### " "/etc/trojan-go/akun.conf")
+let trgo=$trg/2
+
+clear
+# Color Validation
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+BBlue='\e[1;34m'
+BPurple='\e[1;35m'
+BCyan='\e[1;36m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+y='\033[0;34m'
+IPurple='\033[0;95m'
+On_IPurple='\033[0;105m'
+BIPurple='\033[1;95m' 
+
+# // Clear
+
+clear
+cek=$(service ssh status | grep active | cut -d ' ' -f5)
+if [ "$cek" = "active" ]; then
+stat=-f5
+else
+stat=-f7
+fi
+ssh=$(service ssh status | grep active | cut -d ' ' $stat)
+if [ "$ssh" = "active" ]; then
+ressh="${green}ON${NC}"
+else
+ressh="${red}OFF${NC}"
+fi
+sshws=$(service ws-ssl status | grep active | cut -d ' ' $stat)
+if [ "$sshws" = "active" ]; then
+ressshws="${green}ON${NC}"
+else
+ressshws="${red}OFF${NC}"
+fi
+ngx=$(service nginx status | grep active | cut -d ' ' $stat)
+if [ "$ngx" = "active" ]; then
+resngx="${green}ON${NC}"
+else
+resngx="${red}OFF${NC}"
+fi
+dbr=$(service dropbear status | grep active | cut -d ' ' $stat)
+if [ "$dbr" = "active" ]; then
+resdbr="${green}ON${NC}"
+else
+resdbr="${red}OFF${NC}"
+fi
+v2r=$(service xray status | grep active | cut -d ' ' $stat)
+if [ "$v2r" = "active" ]; then
+resv2r="${green}ON${NC}"
+else
+resv2r="${red}OFF${NC}"
+fi
+# STATUS SERVICE UDP CUSTOM
+if [[ $udpc == "running" ]]; then
+   udp2="${GB}[ ON ]${NC}${NC}"
+else
+   udp2="${RB}[ OFF ]${NC}"
+fi
+# VPS Information
+#Domain
+domain=$(cat /etc/xray/domain)
+#Status certificate
+modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
+modifyTime1=$(date +%s -d "${modifyTime}")
+currentTime=$(date +%s)
+stampDiff=$(expr ${currentTime} - ${modifyTime1})
+days=$(expr ${stampDiff} / 86400)
+remainingDays=$(expr 90 - ${days})
+tlsStatus=${remainingDays}
+if [[ ${remainingDays} -le 0 ]]; then
+	tlsStatus="expired"
+fi
+# OS Uptime
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+# Download
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+# user
+Exp2=$"Lifetime"
+Name=$"givpn"
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
+#ISP=$(curl -s ipinfo.io/org?token=ce3da57536810d | cut -d " " -f 2-10 )
+#CITY=$(curl -s ipinfo.io/city?token=ce3da57536810d )
+#WKT=$(curl -s ipinfo.io/timezone?token=ce3da57536810d )
+DAY=$(date +%A)
+DATE=$(date +%m/%d/%Y)
+DATE2=$(date -R | cut -d " " -f -5)
+IPVPS=$(curl -s ifconfig.me )
+LOC=$(curl -s ifconfig.co/country )
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+tram=$( free -m | awk 'NR==2 {print $2}' )
+uram=$( free -m | awk 'NR==2 {print $3}' )
+fram=$( free -m | awk 'NR==2 {print $4}' )
+clear 
+echo -e "${y}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\\E[40;1;37m      SCRIPT MULTIPORT BY RVPN STORES               \E[0m"
+echo -e "${y}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "☞ OS                  = "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+echo -e "☞ Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )"
+echo -e "☞ Current Domain      = $( cat /etc/xray/domain )"
+echo -e "☞ Server IP           = $ip_vps"
+echo -e "☞ Country             = $LOC"
+echo -e "☞ Server Resource     = RAM = $persenmemori% | CPU = $persencpu%"
+echo -e "☞ Clients Name        = RVPNSTORES"
+echo -e "☞ Expired Script VPS  = LIFETIME"
+echo -e ""
+echo -e "┌────────────────────────────────────────────────────┐${NC}"
+echo -e "  ${ORANGE} SSH ${NC}: $ressh"" ${yell} NGINX ${NC}: $resngx"" ${yell}  XRAY ${NC}: $resv2r"" ${yell} TROJAN ${NC}: $resv2r"
+echo -e "  ${yell}     STUNNEL ${NC}: $ressh" "${yell} DROPBEAR ${NC}: $resdbr" "${yell} UDP-CUSTOM ${NC}: $resv2r"    
+echo -e "${y}┌──────────────────────────────────────────────────┐${NC}"
+echo -e "${y}│  \033[0m ${IPurple}${YELLOW}SSH     VMESS       VLESS      TROJAN       SHADOWSOCKS$NC  $COLOR1"
+echo -e "${y}│  \033[0m ${Blue} $ssh1        $vma           $vla          $tra               $ssa   $NC    $COLOR1"
+echo -e "${y}└──────────────────────────────────────────────────┘${NC}"
+echo -e "${y}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\\E[40;1;37m                  MENU SERVICE               \E[0m"
+echo -e "${y}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\\E[40;1;37m                     V.2.8              \E[0m"
+echo -e "┌─────────────────────────────────────────────────────┐${NC}"
+echo -e "   ${BICyan}[${BIGreen}1${BICyan}]${BLUE} SSH ${NC}" 
+echo -e "   ${BICyan}[${BIGreen}2${BICyan}]${BLUE} VMESS ${NC}"    
+echo -e "   ${BICyan}[${BIGreen}3${BICyan}]${BLUE} VLESS ${NC}"    
+echo -e "   ${BICyan}[${BIGreen}4${BICyan}]${BLUE} TROJAN ${NC}" 
+echo -e "   ${BICyan}[${BIGreen}5${BICyan}]${BLUE} SHADOWSOCKS ${NC}"
+echo -e "   ${BICyan}[${BIGreen}6${BICyan}]${BLUE} SYSTEM ${NC}"
+echo -e "└─────────────────────────────────────────────────────┘${NC}"
+echo -e ""
+read -p " Select From Options [1-6 or x ] :  "  opt
+echo -e   ""
+case $opt in
+1) clear ; m-sshovpn ;;
+2) clear ; m-vmess ;;
+3) clear ; m-vless ;;
+4) clear ; m-trojan ;;
+5) clear ; m-ssws ;;
+6) clear ; m-system ;;
+7) clear ; nano /usr/bin/backup ;;
+x) exit ;;
+*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
+esac
