d33edcb4ced0dda73058c81ab25b97d8ade858f5
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-29 06:17:07 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -3,4 +3,6 @@
 echo -e "Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh" && chmod +x /usr/bin/menu
+wget https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/xray/cek-tr.sh
+wget https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/xray/cek-ws.sh
 sleep 2
