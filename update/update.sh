16b14386efe6e5f8811df176c48f7995d88feff6
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-30 18:06:55 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -3,7 +3,5 @@
 echo -e "Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh" && chmod +x /usr/bin/menu
-wget https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/xray/cek-tr.sh
-wget https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/xray/cek-ws.sh
-wget -O m-trojan "https://raw.githubusercontent.com/rizkyckj/AutoScriptXray/master/menu/m-trojan.sh"
+wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-trojan.sh
 sleep 2
