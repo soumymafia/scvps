3c3cabe60364229799e30eaae6182455381dbd23
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-30 13:41:35 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -296,6 +296,8 @@ wget -O running "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/me
 wget -O clearcache "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/clearcache.sh"
 wget -O m-ssws "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-ssws.sh"
 wget -O m-trojan "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-trojan.sh"
+wget -O backup "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/restore.sh"
 
 # menu ssh ovpn
 wget -O m-sshovpn "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-sshovpn.sh"
