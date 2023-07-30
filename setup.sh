e3864627f9c79ac26d195709d5d3052b4cc82a42
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-30 12:21:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 # domain random
-CDN="https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/cf"
+CDN="rvpnstores.com"
 cd /root
 #System version number
 if [ "${EUID}" -ne 0 ]; then
