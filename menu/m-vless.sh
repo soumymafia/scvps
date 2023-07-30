48090d4aae22aa4bf66a3d3208028e768f978822
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-30 18:25:08 +0700

    Update m-vless.sh

--- menu/m-vless.sh
+++ menu/m-vless.sh
@@ -1,32 +1,62 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
-vlx=$(grep -c -e "^#& " "/etc/xray/config.json")
-let vla=$vlx/2
-vmc=$(grep -c -e "^### " "/etc/xray/config.json")
-let vma=$vmc/2
-trx=$(grep -c -E "^#! " "/etc/xray/config.json")
-let tra=$trx/2
-ssx=$(grep -c -E "### " "/etc/trojan-go/akun.conf")
-let ssa=$ssx/2
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-eecho -e "      VMESS          TROJAN          VLESS"
-echo -e "       $vma           $tra            $vla"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+y='\033[0;34m'
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+echo -e "${y}┌━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┐${NC}"
+echo -e "  \\E[40;1;37m[ Xray/Vless Menu ] \E[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Create Account Vless "
-echo -e " [\e[36m•2\e[0m] Trial Account Vless "
-echo -e " [\e[36m•3\e[0m] Extending Account Vless "
-echo -e " [\e[36m•4\e[0m] Delete Account Vless "
-echo -e " [\e[36m•5\e[0m] Check User Login Vless "
-echo -e " [\e[36m•6\e[0m] User list created Account "
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "  \\E[40;1;37m [1] Create Xray/Vless \E[0m"
+echo -e "  \\E[40;1;37m [2] Trial Xray/Vless  \E[0m"
+echo -e "  \\E[40;1;37m [3] Renew Xray/Vless \E[0m"
+echo -e "  \\E[40;1;37m [4] Delete Xray/Vless \E[0m"
+echo -e "  \\E[40;1;37m [5] Cek User Xray/Vless \E[0m"
+echo -e "  \\E[40;1;37m [6] List Create Xray/Vless \E[0m"
+echo -e "${y}└━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┘${NC}"
 echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
