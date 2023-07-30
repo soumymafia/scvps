ddf32e154caa572beb3653c394697a1aea873ac4
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-30 18:56:35 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -2,7 +2,7 @@
 #clear
 echo -e "Downloading Update File"
 sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh
 wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-trojan.sh
 wget -q -O /usr/bin/m-vmess "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-vmess.sh
 wget -q -O /usr/bin/m-vless "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-vless.sh
