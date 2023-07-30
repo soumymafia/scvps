50d36415d79ce03e62c5440a8b5d0fe804325490
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-30 18:27:23 +0700

    Update m-ssws.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -2,23 +2,63 @@
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      • Shadowsocks Account •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+y='\033[0;34m'
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+echo -e "${y}┌━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┐${NC}"
+echo -e "  \\E[40;1;37m[ Xray/Shadowsocks Menu ] \E[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Create Account Shadowsocks "
-echo -e " [\e[36m•2\e[0m] Create Trial Shadowsocks "
-echo -e " [\e[36m•3\e[0m] Extending Account Shadowsocks "
-echo -e " [\e[36m•4\e[0m] Delete Account Shadowsocks "
-echo -e " [\e[36m•5\e[0m] User list created Account "
+echo -e "  \\E[40;1;37m [1] Create Xray/Shadowsocks \E[0m"
+echo -e "  \\E[40;1;37m [2] Trial Xray/Shadowsocks  \E[0m"
+echo -e "  \\E[40;1;37m [3] Renew Xray/Shadowsocks \E[0m"
+echo -e "  \\E[40;1;37m [4] Delete Xray/Shadowsocks \E[0m"
+echo -e "  \\E[40;1;37m [5] Cek User Xray/Shadowsocks \E[0m"
+echo -e "  \\E[40;1;37m [6] List Create Xray/Shadowsocks \E[0m"
+echo -e "${y}└━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┘${NC}"
 echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p " Select menu : " opt
+read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
 1) clear ; add-ssws ;;
