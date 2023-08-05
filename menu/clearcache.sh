9bc7d820c364c54c9743f7ea85129411ab08b770
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-05 13:40:45 +0700

    Update clearcache.sh

--- menu/clearcache.sh
+++ menu/clearcache.sh
@@ -4,7 +4,7 @@ echo "Checking VPS"
 clear
 echo ""
 echo ""
-echo -e "[ \033[32mInfo\033[0m ] Clear RAM Cache"
+echo -e "[ \033[32mInfo\033[0m ] Clear 🧹 "
 echo 1 > /proc/sys/vm/drop_caches
 sleep 3
 echo -e "[ \033[32mok\033[0m ] Cache cleared"
