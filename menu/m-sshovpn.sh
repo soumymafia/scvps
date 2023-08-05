4e12153594eb52152623c82e7a86e58be63bffa6
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-05 12:58:17 +0700

    Update m-sshovpn.sh

--- menu/m-sshovpn.sh
+++ menu/m-sshovpn.sh
@@ -1,29 +1,102 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
+#link izin ip vps
+url_izin='https://raw.githubusercontent.com/rizkyckj/izin/master/izin'
+
+#IP VPS
+ip_vps=$(curl -sS ifconfig.me)
+
+# Mendapatkan isi file izin.txt dari URL
+izin=$(curl -s "$url_izin")
+
+# Memeriksa apakah konten izin.txt berhasil didapatkan
+if [[ -n "$izin" ]]; then
+  while IFS= read -r line; do
+    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
+    nama=$(echo "$line" | awk '{print $1}')
+    ipvps=$(echo "$line" | awk '{print $2}')
+    tanggal=$(echo "$line" | awk '{print $3}')
+
+    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
+    if [[ "$ipvps" == "$ip_vps" ]]; then
+      echo "Nama VPS: $nama"
+      echo "IP VPS: $ipvps"
+      echo "Tanggal Kadaluwarsa: $tanggal"
+      break
+    fi
+  done <<< "$izin"
+
+  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
+  if [[ "$ipvps" != "$ip_vps" ]]; then
+    echo "IP VPS tidak ditemukan dalam izin.txt"
+    exit 0
+  fi
+else
+  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
+  exit 0
+fi
+
 clear
-# CARI APA
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m       • SSH MENU •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+#color
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+y='\033[0;34m'
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+echo -e "${y}┌━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┐${NC}"
+echo -e "  \\E[40;1;37m[ SSH ] \E[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Create SSH & WS Account "
-echo -e " [\e[36m•2\e[0m] Trial SSH & WS Account "
-echo -e " [\e[36m•3\e[0m] Renew SSH & WS Account "
-echo -e " [\e[36m•4\e[0m] Delete SSH & WS Account "
-echo -e " [\e[36m•5\e[0m] Check User Login SSH & WS "
-echo -e " [\e[36m•6\e[0m] List Member SSH & WS "
-echo -e " [\e[36m•7\e[0m] Delete User Expired SSH & WS "
-echo -e " [\e[36m•8\e[0m] Set up Autokill SSH "
-echo -e " [\e[36m•9\e[0m] Cek Users Who Do Multi Login Multi"
-echo -e " [\e[36m•10\e[0m] User list created Account "
-echo -e " [\e[36m•11\e[0m] Change Banner SSH "
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "  \\E[40;1;37m [1] Create SSH \E[0m"
+echo -e "  \\E[40;1;37m [2] Trial SSH  \E[0m"
+echo -e "  \\E[40;1;37m [3] Renew SSH \E[0m"
+echo -e "  \\E[40;1;37m [4] Delete SSH \E[0m"
+echo -e "  \\E[40;1;37m [5] Cek User SSH \E[0m"
+echo -e "  \\E[40;1;37m [6] List Member SSH \E[0m"
+echo -e "  \\E[40;1;37m [7] Delete User Expired \E[0m"
+echo -e "  \\E[40;1;37m [8] Autokill \E[0m"
+echo -e "  \\E[40;1;37m [9] User Multilogin \E[0m"
+echo -e "${y}└━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┘${NC}"
 echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
@@ -37,9 +110,8 @@ case $opt in
 7) clear ; delete ; exit ;;
 8) clear ; autokill ; exit ;;
 9) clear ; ceklim ; exit ;;
-10) clear ; cat /etc/log-create-ssh.log ; exit ;;
-11) clear ; nano /etc/issue.net ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-sshovpn ;;
 esac
+
