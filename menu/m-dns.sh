4e2476c888c4d07f9444a92f3c665ea999edd39d
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-07 10:13:39 +0700

    Update m-dns.sh

--- menu/m-dns.sh
+++ menu/m-dns.sh
@@ -37,29 +37,79 @@ case $dns in
 1)
 clear
 echo ""
-read -p "Please Insert DNS : " dns1
-if [ -z $dns1 ]; then
-echo ""
-echo "Please Insert DNS !"
-sleep 1
-clear
-dns
+DOMAIN=vipme.my.id
+SUB_DOMAIN=${sub}.vipme.my.id
+NS_DOMAIN=slowdns-${subsl}.vipme.my.id
+CF_ID=andyyuda41@gmail.com
+CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+set -euo pipefail
+IP=$(curl -sS ifconfig.me);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '\''{"type":"A","name":"'\''${SUB_DOMAIN}'\''","content":"'\''${IP}'\''","ttl":120,"proxied":false}'\'' | jq -r .result.id)
 fi
-rm /etc/resolv.conf
-rm /etc/resolvconf/resolv.conf.d/head
-touch /etc/resolv.conf
-touch /etc/resolvconf/resolv.conf.d/head
-echo "$dns1" > /root/dns
-echo "nameserver $dns1" >> /etc/resolv.conf
-echo "nameserver $dns1" >> /etc/resolvconf/resolv.conf.d/head
-systemctl restart resolvconf.service
-echo ""
-echo -e "\e[032;1mDNS $dns1 sucessfully insert in VPS\e[0m"
-echo ""
-cat /etc/resolvconf/resolv.conf.d/head
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '\''{"type":"A","name":"'\''${SUB_DOMAIN}'\''","content":"'\''${IP}'\''","ttl":120,"proxied":false}'\'')
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '\''{"type":"NS","name":"'\''${NS_DOMAIN}'\''","content":"'\''${SUB_DOMAIN}'\''","ttl":120,"proxied":false}'\'' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '\''{"type":"NS","name":"'\''${NS_DOMAIN}'\''","content":"'\''${SUB_DOMAIN}'\''","ttl":120,"proxied":false}'\'')
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+echo "Host NS : $NS_DOMAIN"
+echo $NS_DOMAIN > /root/nsdomain
+echo "IP=$SUB_DOMAIN" > /var/lib/scrz-prem/ipvps.conf
 sleep 1
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Domain added.."
+sleep 3
+domain=$(cat /root/domain)
+cp -r /root/domain /etc/xray/domain
 clear
-dns
+echo -e "[ ${GREEN}INFO${NC} ] Starting renew cert... "
+sleep 2
+echo -e "${OKEY} Starting Generating Certificate"
+rm -fr /root/.acme.sh
+mkdir -p /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 ;;
 2)
 clear
