501f4498c4403f89dbc9f4dc0f7b04f52ea4ce20
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-09 23:46:34 +0700

    Add files via upload

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,68 +1,31 @@
 #!/bin/bash
-# Arz
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
+
 clear
-IP=$(wget -qO- ipinfo.io/ip);
-date=$(date +"%Y-%m-%d")
-clear
-email=$(cat /home/rohanah772@gmail.com)
-if [[ "$email" = "" ]]; then
-echo "Masukkan Email Untuk Menerima Backup"
-read -rp "Email : " -e rohanah772@gmail.com
-cat <<EOF>>/home/rohanah772@gmail.com
-$email
-EOF
-fi
-clear
-echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
+#IP=$(wget -qO- ipinfo.io/ip);
+domain=$(cat "/etc/xray/domain")
+date=$(date +"%d-%m-%Y")
+time=$(date +"%H-%M")
+
+echo "Please wait, processing the backup"
 rm -rf /root/backup
 mkdir /root/backup
-cp -r /root/.acme.sh /root/backup/ &> /dev/null
-cp /etc/passwd /root/backup/ &> /dev/null
-cp /etc/group /root/backup/ &> /dev/null
-cp /etc/shadow /root/backup/ &> /dev/null
-cp /etc/gshadow /root/backup/ &> /dev/null
-cp /etc/ppp/chap-secrets /root/backup/chap-secrets &> /dev/null
-cp /etc/ipsec.d/passwd /root/backup/passwd1 &> /dev/null
-cp -r /var/lib/scrz-prem/ /root/backup/scrz-prem &> /dev/null
-cp -r /etc/xray /root/backup/xray &> /dev/null
-cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cp /etc/crontab backup/
+#cp -r /var/lib/alexxa-pro/ backup/alexxa-pro
+cp -r /etc/xray backup/xray
+cp -r /home/vps/public_html backup/public_html
 cd /root
-zip -r $IP-$date.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date.zip dr:backup/
-url=$(rclone link dr:backup/$IP-$date.zip)
-id=(`echo $url | grep '^https' | cut -d'=' -f2`)
-link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "
-Detail Backup WINGS AutoScript
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-" | mail -s "Backup Data" $email
-rm -rf /root/backup
-rm -r /root/$IP-$date.zip
-clear
-echo -e "
-Detail Backup WINGS AutoScript
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-"
-echo
-read -n 1 -s -r -p "   Press any key to back on menu"
-menu
+zip -r /root/backup/${domain}_${date}_${time}.zip backup > /dev/null 2>&1
+sleep 2
+echo "Please wait, preparing the backup (done)"; sleep 1
+
+#============================================
+
+echo "Sending to current telegram chat id"; sleep 2
+python3 /etc/lukman/send.py
+echo "Sending to current telegram chat id (done)"
+
+read -p "Press enter to go back" -n 1; menu-backup
--- backup/restore.sh
+++ backup/restore.sh
@@ -1,64 +1,34 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
+
 clear
-echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
-echo "Please input link to your vps data backup file."
-echo "You can check it on your email if you run backup data vps before."
-read -rp "Link File: " -e url
-wget -O backup.zip "$url"
+echo "Please make sure you have the link from google drive
+and give access to \"anyone with the link\""
+echo -ne "Link: "; read link
+
+full_link="$link"
+# Menggunakan 'sed' untuk mengekstrak ID dari URL
+id=$(echo "$full_link" | sed -n 's/.*\/d\/\([^\/]*\)\/.*/\1/p')
+echo "Extracted ID: $id"
+
+newlink="https://drive.google.com/u/4/uc?id=${id}&export=download"
+cd
+wget -q -O backup.zip "$newlink"
+
+echo Start Restore
 unzip backup.zip
 rm -f backup.zip
 sleep 1
-echo Start Restore
-rm -f /root/backup/backup.zip &> /dev/null
-sleep 1
 cd /root/backup
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring passwd data..."
-sleep 1
-cp /root/backup/passwd /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring group data..."
-sleep 1
-cp /root/backup/group /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring shadow data..."
-sleep 1
-cp /root/backup/shadow /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring gshadow data..."
-sleep 1
-cp /root/backup/gshadow /etc/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring chap-secrets data..."
-sleep 1
-cp /root/backup/chap-secrets /etc/ppp/ &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring passwd1 data..."
-sleep 1
-cp /root/backup/passwd1 /etc/ipsec.d/passwd &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring ss.conf data..."
-sleep 1
-cp /root/backup/ss.conf /etc/shadowsocks-libev/ss.conf &> /dev/null
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring admin data..."
-sleep 1
-cp -r /root/backup/scrz-prem /var/lib/ &> /dev/null
-cp -r /root/backup/.acme.sh /root/ &> /dev/null
-cp -r /root/backup/arzvpn /etc/ &> /dev/null
-cp -r /root/backup/xray /etc/ &> /dev/null
-cp -r /root/backup/public_html /home/vps/ &> /dev/null
-cp -r /root/backup/crontab /etc/ &> /dev/null
-cp -r /root/backup/cron.d /etc/ &> /dev/null
-rm -rf /root/backup &> /dev/null
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+#cp -r alexxa-pro /var/lib/
+cp -r xray /etc/
+cp -r public_html /home/vps/
+cp crontab /etc/
 rm -rf /root/backup
-echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Done... Successfully"
 rm -f backup.zip
 echo Done
-echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
-menu
+
+echo "Note: You need to restart all services (go to settings) to take effect"; sleep 5
