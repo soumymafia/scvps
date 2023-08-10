e11697c7001a90efe0d8a1e65377ca3321cf501f
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-10 12:36:43 +0700

    Update autobackup.sh

--- backup/autobackup.sh
+++ backup/autobackup.sh
@@ -2,7 +2,6 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-colornow=$(cat /etc/yudhynetwork/theme/color.conf)
 NC="\e[0m"
 RED="\033[0;31m"
 GREEN="\033[0;32m" 
