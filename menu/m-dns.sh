b25912ea53adb1f04db947e5f387448d0fcee10f
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-10 12:39:11 +0700

    Update m-dns.sh

--- menu/m-dns.sh
+++ menu/m-dns.sh
@@ -2,11 +2,8 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-colornow=$(cat /etc/yudhynetwork/theme/color.conf)
 NC="\e[0m"
 RED="\033[0;31m" 
-COLOR1="$(cat /etc/yudhynetwork/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/yudhynetwork/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"  
 WH='\033[1;37m'                  
 ###########- RVPN STORES -##########
 
@@ -39,7 +36,7 @@ Chat me at telegram @RVPNSTORES if you encounter any errors
 Thank you for using this script
 ===================================================================================="
 echo ""
-read -p "Press enter to go back" -n 1; menu-backup
+read -p "Press enter to go back" -n 1; m-dns
 }
 
 
