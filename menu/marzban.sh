744e2fec17ac99d1c88d6db991837847d7a1699a
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-12 15:24:59 +0700

    Update marzban.sh

--- menu/marzban.sh
+++ menu/marzban.sh
@@ -14,7 +14,7 @@ echo -e "$COLOR1 ${NC} ${COLBG1}             ${WH}• MARZBAN MENU •          
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
 echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}INSTALL BOT SSH  [ OS UBUNTU ] $NC"
-echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}INSTALL MARZBAN  [ OS UBUNTU ] $NC"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH}INSTALL MARZBAN  [ OS UBUNTU ] $NC"
 echo -e " $COLOR1 $NC                                               $COLOR1 $NC"                                              
 echo -e " $COLOR1 $NC   ${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}GO BACK${NC}                              $COLOR1 $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
@@ -25,7 +25,7 @@ echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
 case $opt in
 01 | 1) clear ; apt update && apt install wget -y && wget https://raw.githubusercontent.com/rizkyckj/xolpanel/master/xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh ;;
-01 | 1) clear ; wget https://raw.githubusercontent.com/Agunxzzz/MarXray/main/sslmar.sh && chmod 755 sslmar.sh && ./sslmar.sh ;;
+02 | 2) clear ; wget https://raw.githubusercontent.com/Agunxzzz/MarXray/main/sslmar.sh && chmod 755 sslmar.sh && ./sslmar.sh ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu ;;
 esac
