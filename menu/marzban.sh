1270bd0ca6c2e534199938ab969078ce90b3fe1e
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-12 14:20:02 +0700

    Update marzban.sh

--- menu/marzban.sh
+++ menu/marzban.sh
@@ -10,10 +10,11 @@ WH='\033[1;37m'
 
 clear
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1 ${NC} ${COLBG1}             ${WH}• XOLPANEL MENU •             ${NC} $COLOR1 $NC"
+echo -e "$COLOR1 ${NC} ${COLBG1}             ${WH}• MARZBAN MENU •             ${NC} $COLOR1 $NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
-echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}INSTALL XOLPANEL [ HANYA BERJALAN DI OS UBUNTU ] $NC"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}INSTALL BOT SSH  [ HANYA BERJALAN DI OS UBUNTU ] $NC"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}INSTALL MARZBAN  [ HANYA BERJALAN DI OS UBUNTU ] $NC"
 echo -e " $COLOR1 $NC                                               $COLOR1 $NC"                                              
 echo -e " $COLOR1 $NC   ${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}GO BACK${NC}                              $COLOR1 $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
@@ -24,6 +25,7 @@ echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
 case $opt in
 01 | 1) clear ; apt update && apt install wget -y && wget https://raw.githubusercontent.com/rizkyckj/xolpanel/master/xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh ;;
+01 | 1) clear ; wget https://raw.githubusercontent.com/Agunxzzz/MarXray/main/sslmar.sh && chmod 755 sslmar.sh && ./sslmar.sh ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu ;;
 esac
