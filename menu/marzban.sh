f4b309938e00ae5b274d57d87ecd0a28d1835796
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-08-18 06:33:16 +0700

    Update marzban.sh

--- menu/marzban.sh
+++ menu/marzban.sh
@@ -12,9 +12,7 @@ echo -e "$COLOR1┌────────────────────�
 echo -e "$COLOR1 ${NC} ${COLBG1}             ${WH}• WEB PANEL MENU •             ${NC} $COLOR1 $NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
-echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH} INSTALL BOT SSH ( OS UBUNTU )${NC}                     $COLOR1 $NC"
-echo -e " $COLOR1 $NC   ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH} INSTALL MARZBAN ( 🔥 DILARANG  MENGINSTAL ) ${NC}      $COLOR1 $NC"
-echo -e " $COLOR1 $NC                                               $COLOR1 $NC" 
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH} INSTALL BOT SSH ( OS UBUNTU )${NC}                     $COLOR1 $NC"                                              
 echo -e " $COLOR1 $NC   ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH} SETTING ${NC}                              $COLOR1 $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
@@ -24,8 +22,7 @@ echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
 case $opt in
 01 | 1) clear ; apt update && apt install wget -y && wget https://raw.githubusercontent.com/rizkyckj/xolpanel/master/xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh ;;
-02 | 2) clear ; wget https://raw.githubusercontent.com/Agunxzzz/MarXray/main/sslmar.sh && chmod 755 sslmar.sh && ./sslmar.sh ;;
-03 | 3) clear ; m-system ;;
+02 | 2) clear ; m-system ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu-backup ;;
 esac
